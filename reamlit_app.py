e5fc55a9027e6536a36599b63c2384e1299b0631
kaamd <mkd_kaktus@vk.com>
2025-04-17 23:00:09 +0500

    Update reamlit_app.py

--- reamlit_app.py
+++ reamlit_app.py
@@ -2,7 +2,7 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
-# Show the page title and description
+# Show the page title and description.
 st.set_page_config(page_title="Book dataset", page_icon="📚")
 st.title("📚 Book dataset")
 st.write(
@@ -11,7 +11,7 @@ st.write(
     """
 )
 
-# Load the data from a CSV
+# Load the data from a CSV.
 @st.cache_data
 def load_data():
     df = pd.read_csv("japanese_books (1).csv")
@@ -39,8 +39,14 @@ authors = st.multiselect(
 # Фильтрация DataFrame по выбранным авторам и выбор только необходимых колонок
 df_filtered = df[df["author"].isin(authors)][["author", "title"]].reset_index(drop=True)
 
-# Переформатирование DataFrame в сводную таблицу без столбца с номерами
-df_reshaped = df_filtered.pivot(columns='author', values='title').fillna('').reset_index(drop=False)
+# Добавление столбца с порядковыми номерами, который будет независим от авторов
+df_filtered['№'] = range(1, len(df_filtered) + 1)
+
+# Переформатирование DataFrame в сводную таблицу
+df_reshaped = df_filtered.pivot(index='№', columns='author', values='title').fillna('').reset_index(drop=False)
+
+# Переименование индекса
+df_reshaped.index.name = '№'
 
 # Настройка стиля таблицы
 st.markdown(
@@ -70,13 +76,13 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Отображение таблицы без номера авторов
+# Отображение таблицы
 st.dataframe(
     df_reshaped.style.set_table_attributes('class="streamlit-table"'),
     use_container_width=True,
 )
 
-# Prepare data for the bar chart
+# Prepare data for the bar chart.
 df_chart = df_filtered.groupby(['author', 'title']).size().reset_index(name='count')
 
 # Предполагаем, что count является целым числом
@@ -92,5 +98,5 @@ chart = chart.encode(
     x=alt.X('sum(count):Q', title='Количество книг', axis=alt.Axis(format='d', ticks=True, grid=False, values=[0, 1, 2, 3, 4, 5]))  # Указать значения, которые хотим видеть на оси X
 )
 
-# Display the data as a bar chart using `st.altair_chart`
+# Display the data as a bar chart using `st.altair_chart`.
 st.altair_chart(chart, use_container_width=True)
